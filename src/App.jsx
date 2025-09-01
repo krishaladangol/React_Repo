daab59148ccf642290eca10adf07aa4553ea2ce2
Krishala Dangol <Krishaladangol16@gmail.com>
2025-09-01 22:40:04 +0545

    css styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Header from './Firstpage.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
-
+import Button from './Button/Button.jsx'
+import Buttons from './inlinecss.jsx'
 function App() {
   return (
     <>template tag
@@ -12,6 +13,8 @@ function App() {
    <Card/>
    <Card/>
    <Card/>
+   <Button/>\
+   <Buttons/>
    </>
 
   );
---
+++ src/inlinecss.jsx
@@ -0,0 +1,16 @@
+function Buttons(){
+    const styles={
+        color:"Red",
+        backgroundColor:"skyblue",
+
+        cursor:"pointer",
+    }
+    return(
+        <>
+        <button style={styles}>Inline Button</button>
+        
+        </>
+    );
+   
+}
+ export default Buttons;
